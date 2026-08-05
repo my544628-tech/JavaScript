1f079511133fb29fe74d551125d83edfc2e7ff55
Mohamed Abdi <dhageey155@hotmail.com>
2026-08-05 20:47:29 +0300

    first commit

---
+++ ch1_intro/script.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+x = 5
+y = 3
+total = x * y
+console.log("the result is", total)
+console.log("Mohamed")
+
---
+++ ch2_Variable_datatypes/script.js
@@ -0,0 +1,25 @@
+// var firstName = "Mohamed" // string 
+// var lastName = "Abdulle" // string
+// var age = 20 // number
+// var isStudent = true // boolean
+// var friends = ["Muse", "Ali"] // 
+// var townOfBirth = "Mogadishu"
+// console.log(townOfBirth)
+// console.log(firstName * lastName)
+// // console.log(age * 5)
+// console.log(firstName.toUpperCase())
+// console.log(age.toUpperCase())
+// console.log(firstName, lastName);
+
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName); 
+
+
+// var town = "London"
+// town = "paris"
+// console.log(town)
+var town = 'london'
+console.log(town)
