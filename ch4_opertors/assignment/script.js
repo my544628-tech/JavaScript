b265853bc25785d8695432841f758e92aad6ff68
Mohamed Abdi <dhageey155@hotmail.com>
2026-08-26 09:25:54 +0300

    I learn about operators

---
+++ ch4_opertors/assignment/script.js
@@ -0,0 +1,17 @@
+var age = 20
+console.log(age);
+// age = age + 5
+// age += 5
+// console.log(age);
+age = age - 5
+age -= 5
+console.log(age);
+
+age *= 5
+console.log(age);
+
+age /= 5
+console.log(age);
+
+age %= 5
+console.log(age);
---
+++ ch4_opertors/logical/script.js
@@ -0,0 +1,5 @@
+var age = 20
+console.log(age);
+console.log(age > 0 && age < 2);
+console.log(age > 0 || age < 2);
+console.log(!true);
