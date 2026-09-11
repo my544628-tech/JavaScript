ff47fa9e975e391e1eee22dbfaff7c1a8fe44d40
Mohamed Abdi <dhageey155@hotmail.com>
2026-09-11 09:33:24 +0300

    I added some while loops

--- ch9_loops/script.js
+++ ch9_loops/script.js
@@ -6,20 +6,51 @@ console.log("Kenya");
 
 console.log("Asia");
 // console.log("china");
+// for ( let counter = 0; counter < 5; counter++) {
+//    if (counter === 1) {
+//       // break;
+//       continue;
+//    }
+//       console.log( counter,"china");
 
- for (let counter = 0, counter < 6, counter++) {
-    console.log("china");
- }
+// }
 
-console.log("India");
+// console.log("India");
+for (let counter = 0; counter < 5; counter++){
+   // for (counter === 2) {
+      // continue
+   // }
+      console.log(counter,"india");
+
+}
+   
+
+
 console.log("Japan");
 
 console.log("Europe");
+let counter = 0;
+while (counter < 5) {
+  console.log("Germany");
+  counter++;
+}
+
 console.log("Uk");
-console.log("Germany");
 console.log("France");
 
 console.log("North America");
 console.log("United state");
 console.log("Mexico");
 console.log("Canada");
+// for (let counter = 0;counter < 5; counter++ )
+
+
+   let students = ["joseph","Ali","john"];
+// for (const key in object) {
+//    if (!Object.hasOwn(object, key)) continue;
+   
+//    const element = object[key];
+// }
+// for (let element of students){
+//     console.log(element);
+// }
